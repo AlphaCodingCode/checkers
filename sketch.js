29608fad3fc91c6a50fed18ffd697b7edf5e53f9
AlphaCodingcode <dilshan@alphacoding.com>
2019-07-29 12:37:15 -0400

    init basejs

---
+++ sketch.js
@@ -0,0 +1,17 @@
+// Any global variables can be defined up here
+
+
+/*
+    Code in the setup function will only be run once at the start of the animation
+*/
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+}
+
+/*
+    The draw function is executed once per frame.
+*/
+function draw() {
+    // Update
+    // Render
+}
